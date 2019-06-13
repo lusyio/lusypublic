293cddb341d83afe0b4ce378979b4d2545a260c5
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2019-06-13 09:29:31 +0300

    Первая верстка

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php include 'header.php';?>
+
+<div class="main">
+	<div class="container">
+		<div class="row justify-content-sm-center">
+			<div class="col-sm-8 text-center">
+				<p class="lead-text">Освободите место для великих свершений</p>
+				<h1>Lusy.io - система постановки и контроля задач</h1>
+				<a href="https://s.lusy.io/reg/" class="btn btn-primary mt-3">Начать пользоваться</a>
+			</div>
+			<div class="col-sm-12">
+				<img class="w-100" src="/images/lusy.jpg">
+			</div>
+		</div>
+	</div>
+</div>
+<div class="second pt-5 pb-5">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 d-none">
+				Ваша компания будет расти, только если вы будете думать о ее развитии. Погружаясь в рутину, нет движения. Можно покзаать путь к решение рутины - шаг первый это внедрить систему управления зарегав сотрудников. Второй шаг - постановка задач и контроль сроков - теперь голова свободна. Учет событий мотивация -  люди работают вы спокойны и движетесь вперед. В итоге вы имеет контроль над процессами в телефоне, не думаете о напоминаии сортудникам и ваш взор направлен вперед, где вашу команию ждет успех.
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container pt-5 pb-5">
+	<div class="text-center d-none">
+		<h2>Что может вам на пути к успеху</h2>
+		<p>Перечень функций</p>
+	</div>
+</div>
+<div class="second pt-5 pb-5">
+	<div class="container">
+		<div class="text-center d-none">
+			<h2>Сколько это стоит?</h2>
+			<p>2 тарифа с человеческим объяснением ценообразования</p>
+		</div>
+	</div>
+</div>
+<div class="container pt-5 pb-5">
+	<div class="text-center d-none">
+		<h2>Письмо от основателя</h2>
+		<p>Перечень функций</p>
+	</div>
+</div>
+<?php include 'footer.php';?>
