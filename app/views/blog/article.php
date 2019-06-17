2152645f6268d2d6bb9491f78ef80c8def6e8622
Kelevra <kelevra@MacBook-Pro-Dmitrij-2.local>
2019-06-17 11:59:43 +0300

    Разметка Schema.org для статей

--- app/views/blog/article.php
+++ app/views/blog/article.php
@@ -1,18 +1,18 @@
-<div class="container article-text">
+<div class="container article-text" itemscope itemtype="https://schema.org/Article">
     <div class="row">
         <div class="col-sm-8 offset-sm-2">
-            <h1 class="font-weight-bold mt-5 mb-3"><?= $article['article_name']; ?></h1>
-            <small><?= $article['category']; ?> | <?= date('d.m.Y', $article['publish_date']); ?></small>
+            <h1 class="font-weight-bold mt-5 mb-3" itemprop="name"><?= $article['article_name']; ?></h1>
+            <small><span itemprop="articleSection"><?= $article['category']; ?></span> | <?= date('d.m.Y', $article['publish_date']); ?><span itemprop="datePublished" class="d-none"><?= date('Y-m-d', $article['publish_date']); ?></span></small>
         </div>
     </div>
     <img src="/public/<?php if (!empty($article['img_full'])) {
         echo 'upload/' . $article['img_full'];
     } else {
         echo 'images/default_full.jpg';
-    } ?>" class="w-100 mt-3 mb-4" title="<?= $article['article_name']; ?>">
+    } ?>" class="w-100 mt-3 mb-4" title="<?= $article['article_name']; ?>" itemprop="image">
 
     <div class="row">
-        <div class="col-sm-8 offset-sm-2">
+        <div class="col-sm-8 offset-sm-2" itemprop="articleBody">
             <?= $article['article_text']; ?>
         </div>
     </div>
