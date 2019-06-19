3c2013b0dabc76f573a07f08bad844a19fc05258
Front <dimkaFPS@gmail.com>
2019-06-19 15:09:38 +0300

    Create index.php

---
+++ app/views/chart/index.php
@@ -0,0 +1,54 @@
+<div class="container">
+    <div class="chart-left float-left">
+        <div class="row">
+            <div class="col">
+                <div class="chart-title">
+                    <h4>Лидеры</h4>
+                </div>
+                <div class="chart-leaderboard">
+
+                </div>
+                <div class="chart-content">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="chart-right float-right">
+        <div class="row">
+            <div class="leaderboard-title col-2">
+                <div class="leaderboard-place">
+                    Место
+                </div>
+                <div class="leaderboard-name col">
+                    Назваание
+                </div>
+                <div class="leaderboard-points col-2">
+                    Очки
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <div class="leaderboard-content">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="company-place">
+
+                            </div>
+                            <div class="company-name">
+
+                            </div>
+                            <div class="company-points">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
