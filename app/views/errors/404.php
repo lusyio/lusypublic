2d4fd34e5459bbbc3773e5bf79bf7f45a06b1fb8
Front <dimkaFPS@gmail.com>
2019-08-07 12:05:22 +0300

    страница 404

--- app/views/errors/404.php
+++ app/views/errors/404.php
@@ -1,1 +1,34 @@
-404
+<link rel="stylesheet" href="/public/css/custom.css?ver=8">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="shortcut icon" href="/public/images/favicon.png" type="image/x-icon">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+<div id="error404">
+    <div class="top-sidebar pt-2 pb-2">
+        <div class="container">
+            <div class="row text-left">
+                <div class="col-12">
+                    <a class="navbar-brand text-dark text-uppercase font-weight-bold visible-lg mt-1" href="/ru/"><span
+                                class="logo mr-3">L</span>LUSY</a>
+                    <p class="mb-0">Система постановки задач</p>
+                </div>
+            </div>
+            <hr>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row text-center">
+            <div class="col">
+                <a href="/ru/">
+                <p class="bg-icon-error"><i class="fas fa-frown-open"></i></p>
+                </a>
+            </div>
+        </div>
+        <div class="row mt-5 text-center">
+            <div class="col">
+                <h1>Упс, что-то пошло не так :(</h1>
+                <p>Если вы хотите начать всё сначала, перейдите на <a href="/ru/">Lusy.io</a></p>
+            </div>
+        </div>
+    </div>
+</div>
